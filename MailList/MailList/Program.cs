d523127a41f39b3d7beb157f32674d3f2933557a
Hovhannes Nalbandyan <hovhannesnalbandyan92@gmail.com>
2015-10-01 13:19:16 +0400

    First Commit

---
+++ MailList/MailList/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadWrite a = new ReadWrite() { Emails = @"C:\Users\HovArp\Documents\Visual Studio 2015\Projects\EmailList\MailingList.txt" };
+            a.Reads();
+            a.Writes();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MailList/MailList/ReadWrite.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace MailList
+{
+    class ReadWrite
+    {
+        public string Emails { get; set; }
+
+
+        public void Reads()
+        {
+            string _users = null;
+            try
+            {
+                StreamReader Reader = new StreamReader(Emails);
+                while ((_users = Reader.ReadLine()) != null)
+                {
+                    if (!string.IsNullOrEmpty(_users))
+                    {
+                        string Names = _users.Split(new char[] { '<' }, StringSplitOptions.RemoveEmptyEntries)[0];
+                        Console.WriteLine(Names);
+                    }
+                }
+                Reader.Close();
+            }
+            catch (FileNotFoundException ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public void Writes()
+        {
+            try
+            {
+                var Writer = new StreamWriter(Emails, true);
+                Writer.WriteLine(Console.ReadLine());
+                Writer.Close();
+
+            }
+            catch (FileNotFoundException Fnot)
+            {
+                Console.WriteLine(Fnot.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
+
